895b28deb5cdcc3ebacd7878daaf532995c2feb5
s1290228 <136041456+s1290228@users.noreply.github.com>
2023-06-15 23:14:54 +0900

    Dice game:initial revision

---
+++ DiceGame.c
@@ -0,0 +1,21 @@
+#include <time.h>
+#include <unistd.h>
+
+int main(){
+  int i,j,r,total;
+  
+  printf("Rolling dice...\n");
+  for(i=1;i<2;i++){
+  srand((unsigned int)time(NULL));
+    for(j=1;j<3;j++){
+      printf("Die %d:",j);
+      r =rand() %6 + 1;
+      printf("%d\n",r);
+      total += r;
+    }
+    printf("Total value: %d\n",total);
+    
+    sleep(1);
+  }
+  return 0;
+}
